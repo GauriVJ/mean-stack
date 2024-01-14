914d9441a94ed19208f7c30f4798ad9ece668bf7
GauriVJ <gvjadhav039@gmail.com>
2024-01-14 12:54:31 +0530

    Added an API for Get and delete  method by id

--- server/api/user/user.route.js
+++ server/api/user/user.route.js
@@ -3,5 +3,9 @@ var router = express.Router();
 const userController = require('./user.controller')
 
 router.get('/', userController.getUsers);
+router.get('/fname', userController.getUsersbyfname);
+router.get('/:id', userController.getUsersbyId);
+router.post('/', userController.createUsers);
+router.delete('/:id', userController.DeleteUsersbyId);
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var usersRouter = require('./api/user/user.route');
 var tasksRouter = require('./api/task/task.route');
-
 var app = express();
+require('dotenv').config;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '*');
   next();
 })
-
+// password is yet to be protected   
 app.use(express.json());
+mongoose.connect("mongodb+srv://gvjadhav039:123Gaurijadhav@cluster0.lwkuhm4.mongodb.net/mydb?retryWrites=true&w=majority").then(() => {
+  console.log('database connection established')
+})
+.catch((err) => {
+  console.log(err);
+  console.log('Error occured while connecting to mongo')
+})
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 
